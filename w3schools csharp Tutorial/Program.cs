e96ad9ddabb3947d85b9007efb2bf516616f29c4
MichaelMagdyG <wall-e84@hotmail.com>
2021-09-28 04:32:06 +0200

    Add project files.

---
+++ w3schools csharp Tutorial/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            string x = "10";
+            string y = "20";
+            string z = x + y;  // z will be 1020 (a string)
+            Console.WriteLine(Convert.ToString(z));
+        }
+    }
+}
